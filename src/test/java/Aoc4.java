7de29a02db13d68a7f956ba6f82d535030a3537f
tommychat <thomas.bennett@caminu.fr>
2022-08-10 16:28:09 +0200

    let's try again

--- src/test/java/Aoc4.java
+++ src/test/java/Aoc4.java
@@ -11,20 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Aoc4 {
     @Test
     void emptyBoardIsEmpty() {
-        String emptyBoard = "";
 
-        String actual = emptyBoard == emptyBoard ? emptyBoard : "a board filled with something";
-
-        assertEquals(actual, emptyBoard);
-    }
-
-    @Test
-    void aBoardWithNumber1IsABoardWithNumber1() {
-        String emptyBoard = "";
-        String board = "1";
-
-        String actual = board == emptyBoard ? emptyBoard : board;
-
-        assertEquals(actual, board);
     }
 }
