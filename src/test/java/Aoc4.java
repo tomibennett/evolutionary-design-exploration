3053d13b8bd5f6d1f5012980c9fbf09432b48ae3
tommychat <thomas.bennett@caminu.fr>
2022-08-10 16:53:05 +0200

    triangulating over board and number concepts

--- src/test/java/Aoc4.java
+++ src/test/java/Aoc4.java
@@ -11,6 +11,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Aoc4 {
     @Test
     void emptyBoardIsEmpty() {
+        final var expected = "empty board";
 
+        final var actual = "empty board" == "empty board" ? "empty board" : "";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void boardWithNumber1IsBoardWithNumber1() {
+        final var expected = "board with number 1";
+
+        final var actual = "board with number 1" == "empty board" ? "empty board" : "board with number 1";
+
+        assertEquals(expected, actual);
     }
 }
