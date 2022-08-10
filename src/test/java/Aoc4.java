fbaf26c7e67726f30e7629ac45563034f2c87105
tommychat <thomas.bennett@caminu.fr>
2022-08-10 16:26:21 +0200

    feat(board): triangulating over board and numbers concepts

--- src/test/java/Aoc4.java
+++ src/test/java/Aoc4.java
@@ -1,8 +1,30 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+// what are the concepts here
+// a board
+// numbers
+// marking and unmarking
+// winning and so
 public class Aoc4 {
     @Test
-    void name() {
+    void emptyBoardIsEmpty() {
+        String emptyBoard = "";
 
+        String actual = emptyBoard == emptyBoard ? emptyBoard : "a board filled with something";
+
+        assertEquals(actual, emptyBoard);
+    }
+
+    @Test
+    void aBoardWithNumber1IsABoardWithNumber1() {
+        String emptyBoard = "";
+        String board = "1";
+
+        String actual = board == emptyBoard ? emptyBoard : board;
+
+        assertEquals(actual, board);
     }
 }
