8be8e556bd7113a066c8f887121d650909e49b15
tommychat <thomas.bennett@caminu.fr>
2022-09-26 16:57:43 +0200

    commentating marking concept to concentrate on the board

--- src/test/java/BingoGame.java
+++ src/test/java/BingoGame.java
@@ -64,126 +64,126 @@ public class BingoGame {
         assertEquals(expected, actual);
     }
 
-    // marking
-    @Test
-    @Disabled
-    void boardWithNumber3NotMarkedAnd2AsNumberToMarkIs3() {
-        final var number1 = "3";
-        final var numberToMark = "2";
-        final var board = number1;
-
-        final var actual = board;
-
-        assertEquals(board, actual);
-    }
-
-    @Test
-    @Disabled
-    void boardWithNumber3NotMarkedAnd3AsNumberToMarkIs3Marked() {
-        final var number1 = "3";
-        final var sep = " ";
-        final var numberToMark = number1;
-        final var board = number1;
-
-        String actual = board;
-
-        if (Objects.equals(numberToMark, number1)) {
-            actual = number1 + sep + "marked";
-        }
-
-        final var expected = number1 + sep + "marked";
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @Disabled
-    void boardWithNumber3NotMarkedAnd4NotMarkedAnd4AsNumberToMarkIs3UnmarkedAnd4Marked() {
-        final var number1 = "3";
-        final var sep = " ";
-        final var number2 = "4";
-        final var numberToMark = number2;
-        final var board = number1 + sep + number2;
-
-        String actual = board;
-
-        if (Objects.equals(numberToMark, number1)) {
-            actual = number1 + sep + "marked";
-        }
-
-        if (Objects.equals(numberToMark, number2)) {
-            actual = number1 + sep + number2 + sep + "marked";
-        }
-
-        final var expected = number1 + sep + number2 + sep + "marked";
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @Disabled
-    void boardWithNumber3NotMarkedAnd4And3AsNumberToMarkIs3MarkedAnd4Unmarked() {
-        final var number1 = "3";
-        final var sep = " ";
-        final var number2 = "4";
-        final var numberToMark = number1;
-        final var board = number1 + sep + number2;
-
-        String actual = board;
-
-        if (Objects.equals(numberToMark, number1)) {
-            actual = number1 + sep + "marked";
-        }
-
-        if (Objects.equals(numberToMark, number2)) {
-            actual = number1 + sep + number2 + sep + "marked";
-        }
-
-        final var expected = number1 + sep + "marked" + sep + number2;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @Disabled
-    void boardWithNumber3NotMarkedAnd4NotMarkedAnd5NotMarkedAnd3AsNumberToMarkIs3MarkedAnd4UnmarkedAnd5NotMarked() {
-        final var number1 = "3";
-        final var sep = " ";
-        final var number2 = "4";
-        final var number3 = "5";
-        final var numberToMark = number1;
-        final var board = number1 + sep + number2 + sep + number3;
-
-        String actual = null;
-
-        if (Objects.equals(board, "")) {
-            actual = "";
-        } else if (Objects.equals(number1, numberToMark)) {
-            actual = number1 + sep + "marked";
-        } else {
-            final var bingoNumbers = board.split(sep);
-
-            if (bingoNumbers.length == 2) {
-                if (Objects.equals(number1, numberToMark)) {
-                    actual = number1 + sep + "marked" + sep + number2;
-                } else if (Objects.equals(number2, numberToMark)) {
-                    actual = number1 + sep + number2 + sep + "marked";
-                }
-            }
-            if (actual == null) {
-                if (bingoNumbers.length == 3) {
-                    if (Objects.equals(number1, numberToMark)) {
-                        actual = number1 + sep + "marked" + sep + number2 + sep + number3;
-                    }
-                }
-                if (actual == null) {
-                    actual = board;
-                }
-            }
-        }
-
-        final var expected = number1 + sep + "marked" + sep + number2 + sep + number3;
-
-        assertEquals(expected, actual);
-    }
+//    // marking
+//    @Test
+//    @Disabled
+//    void boardWithNumber3NotMarkedAnd2AsNumberToMarkIs3() {
+//        final var number1 = "3";
+//        final var numberToMark = "2";
+//        final var board = number1;
+//
+//        final var actual = board;
+//
+//        assertEquals(board, actual);
+//    }
+//
+//    @Test
+//    @Disabled
+//    void boardWithNumber3NotMarkedAnd3AsNumberToMarkIs3Marked() {
+//        final var number1 = "3";
+//        final var sep = " ";
+//        final var numberToMark = number1;
+//        final var board = number1;
+//
+//        String actual = board;
+//
+//        if (Objects.equals(numberToMark, number1)) {
+//            actual = number1 + sep + "marked";
+//        }
+//
+//        final var expected = number1 + sep + "marked";
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    @Disabled
+//    void boardWithNumber3NotMarkedAnd4NotMarkedAnd4AsNumberToMarkIs3UnmarkedAnd4Marked() {
+//        final var number1 = "3";
+//        final var sep = " ";
+//        final var number2 = "4";
+//        final var numberToMark = number2;
+//        final var board = number1 + sep + number2;
+//
+//        String actual = board;
+//
+//        if (Objects.equals(numberToMark, number1)) {
+//            actual = number1 + sep + "marked";
+//        }
+//
+//        if (Objects.equals(numberToMark, number2)) {
+//            actual = number1 + sep + number2 + sep + "marked";
+//        }
+//
+//        final var expected = number1 + sep + number2 + sep + "marked";
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    @Disabled
+//    void boardWithNumber3NotMarkedAnd4And3AsNumberToMarkIs3MarkedAnd4Unmarked() {
+//        final var number1 = "3";
+//        final var sep = " ";
+//        final var number2 = "4";
+//        final var numberToMark = number1;
+//        final var board = number1 + sep + number2;
+//
+//        String actual = board;
+//
+//        if (Objects.equals(numberToMark, number1)) {
+//            actual = number1 + sep + "marked";
+//        }
+//
+//        if (Objects.equals(numberToMark, number2)) {
+//            actual = number1 + sep + number2 + sep + "marked";
+//        }
+//
+//        final var expected = number1 + sep + "marked" + sep + number2;
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    @Disabled
+//    void boardWithNumber3NotMarkedAnd4NotMarkedAnd5NotMarkedAnd3AsNumberToMarkIs3MarkedAnd4UnmarkedAnd5NotMarked() {
+//        final var number1 = "3";
+//        final var sep = " ";
+//        final var number2 = "4";
+//        final var number3 = "5";
+//        final var numberToMark = number1;
+//        final var board = number1 + sep + number2 + sep + number3;
+//
+//        String actual = null;
+//
+//        if (Objects.equals(board, "")) {
+//            actual = "";
+//        } else if (Objects.equals(number1, numberToMark)) {
+//            actual = number1 + sep + "marked";
+//        } else {
+//            final var bingoNumbers = board.split(sep);
+//
+//            if (bingoNumbers.length == 2) {
+//                if (Objects.equals(number1, numberToMark)) {
+//                    actual = number1 + sep + "marked" + sep + number2;
+//                } else if (Objects.equals(number2, numberToMark)) {
+//                    actual = number1 + sep + number2 + sep + "marked";
+//                }
+//            }
+//            if (actual == null) {
+//                if (bingoNumbers.length == 3) {
+//                    if (Objects.equals(number1, numberToMark)) {
+//                        actual = number1 + sep + "marked" + sep + number2 + sep + number3;
+//                    }
+//                }
+//                if (actual == null) {
+//                    actual = board;
+//                }
+//            }
+//        }
+//
+//        final var expected = number1 + sep + "marked" + sep + number2 + sep + number3;
+//
+//        assertEquals(expected, actual);
+//    }
 }
