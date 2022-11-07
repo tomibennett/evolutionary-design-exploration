c860c491fc5cace4eb49bc2a15ad9e4c42fb4d45
tommychat <thomas.bennett@caminu.fr>
2022-11-07 19:28:58 +0100

    cleaning tests

--- src/test/java/Bingo/GameTest.java
+++ src/test/java/Bingo/GameTest.java
@@ -18,19 +18,21 @@ public class GameTest {
     // introducing numbers
     @Test
     void boardWithNumber3IsBoardWithNumber3() {
-        final var bingoBoard = new BoardBuilder()
+        final var board = new BoardBuilder()
             .withNumber(BingoNumber.fromString("3"))
             .build();
 
-        final var expected = new BoardBuilder().withNumber(BingoNumber.fromString("3")).build();
+        final var expected = new BoardBuilder()
+                .withNumber(BingoNumber.fromString("3"))
+                .build();
 
-        assertEquals(expected, bingoBoard);
+        assertEquals(expected, board);
     }
 
     @Test
     void boardWithNumber3And66IsBoardWithNumber3And66() {
 
-        final var bingoBoard = new BoardBuilder()
+        final var board = new BoardBuilder()
             .withNumber(BingoNumber.fromString("3"))
             .withNumber(BingoNumber.fromString("66"))
             .build();
@@ -40,13 +42,13 @@ public class GameTest {
             .withNumber(BingoNumber.fromString("66"))
             .build();
 
-        assertEquals(expected, bingoBoard);
+        assertEquals(expected, board);
     }
 
     @Test
     void boardWithNumber3And66And2987IsBoardWithNumber3And66And2987() {
 
-        final var bingoBoard = new BoardBuilder()
+        final var board = new BoardBuilder()
             .withNumber(BingoNumber.fromString("3"))
             .withNumber(BingoNumber.fromString("66"))
             .withNumber(BingoNumber.fromString("2987"))
@@ -58,148 +60,119 @@ public class GameTest {
             .withNumber(BingoNumber.fromString("2987"))
             .build();
 
-        assertEquals(expected, bingoBoard);
+        assertEquals(expected, board);
     }
 
     @Test
     void onAOneNumberBoardTheNumberDrawnIsPresentOnTheBoardThenItIsMarked() {
-        final var board =
-            new BoardBuilder()
+        final var board = new BoardBuilder()
                 .withNumber(BingoNumber.fromString("39"))
                 .build();
         final var numberDrawn = BingoNumber.fromString("39");
 
-        final var numberMarked = numberDrawn.mark();
         board.mark(numberDrawn);
 
-        final var actual = board;
-
-        final var expected =
-            new BoardBuilder()
-                .withNumber(numberMarked)
+        final var expected = new BoardBuilder()
+                .withNumber(numberDrawn.mark())
                 .build();
 
-        assertEquals(expected, actual);
+        assertEquals(expected, board);
     }
 
     @Test
     void onATwoNumberBoardTheNumberDrawnIsPresentAtFirstPositionOnTheBoardThenItIsMarked() {
-        final var board =
-            new BoardBuilder()
+        final var board = new BoardBuilder()
                 .withNumber(BingoNumber.fromString("92"))
                 .withNumber(BingoNumber.fromString("1234"))
                 .build();
         final var numberDrawn = BingoNumber.fromString("92");
-        final var numberMarked = numberDrawn.mark();
+
         board.mark(numberDrawn);
 
-        final var actual = board;
-
-        final var expected =
-            new BoardBuilder()
-                .withNumber(numberMarked)
+        final var expected = new BoardBuilder()
+                .withNumber(numberDrawn.mark())
                 .withNumber(BingoNumber.fromString("1234"))
                 .build();
 
-        assertEquals(expected, actual);
+        assertEquals(expected, board);
     }
 
     @Test
     void onATwoNumberBoardTheNumberDrawnIsPresentAtSecondPositionOnTheBoardThenItIsMarked() {
-        final var board =
-            new BoardBuilder()
+        final var board = new BoardBuilder()
                 .withNumber(BingoNumber.fromString("38"))
                 .withNumber(BingoNumber.fromString("26"))
                 .build();
         final var numberDrawn = BingoNumber.fromString("26");
 
-        final var numberMarked = numberDrawn.mark();
         board.mark(numberDrawn);
 
-        final var actual = board;
-
-        final var expected =
-            new BoardBuilder()
+        final var expected = new BoardBuilder()
                 .withNumber(BingoNumber.fromString("38"))
-                .withNumber(numberMarked)
+                .withNumber(numberDrawn.mark())
                 .build();
 
-        assertEquals(expected, actual);
+        assertEquals(expected, board);
     }
 
     @Test
     void onAThreeNumberBoardTheNumberDrawnIsPresentAtFirstPositionOnTheBoardThenItIsMarked() {
-        final var board =
-            new BoardBuilder()
+        final var board = new BoardBuilder()
                 .withNumber(BingoNumber.fromString("23"))
                 .withNumber(BingoNumber.fromString("85"))
                 .withNumber(BingoNumber.fromString("246"))
                 .build();
         final var numberDrawn = BingoNumber.fromString("23");
 
-        final var numberMarked = numberDrawn.mark();
         board.mark(numberDrawn);
 
-        final var actual = board;
-
-        final var expected =
-            new BoardBuilder()
-                .withNumber(numberMarked)
+        final var expected = new BoardBuilder()
+                .withNumber(numberDrawn.mark())
                 .withNumber(BingoNumber.fromString("85"))
                 .withNumber(BingoNumber.fromString("246"))
                 .build();
 
-        assertEquals(expected, actual);
+        assertEquals(expected, board);
     }
 
     @Test
     void onAThreeNumberBoardTheNumberDrawnIsPresentAtSecondPositionOnTheBoardThenItIsMarked() {
-        final var board =
-            new BoardBuilder()
+        final var board = new BoardBuilder()
                 .withNumber(BingoNumber.fromString("23"))
                 .withNumber(BingoNumber.fromString("85"))
                 .withNumber(BingoNumber.fromString("246"))
                 .build();
         final var numberDrawn = BingoNumber.fromString("85");
 
-        final var numberMarked = numberDrawn.mark();
         board.mark(numberDrawn);
 
-        final var actual = board;
-
-        final var expected =
-            new BoardBuilder()
+        final var expected = new BoardBuilder()
                 .withNumber(BingoNumber.fromString("23"))
-                .withNumber(numberMarked)
+                .withNumber(numberDrawn.mark())
                 .withNumber(BingoNumber.fromString("246"))
                 .build();
 
-        assertEquals(expected, actual);
+        assertEquals(expected, board);
     }
 
     @Test
     void onAThreeNumberBoardTheNumberDrawnIsPresentAtThirdPositionOnTheBoardThenItIsMarked() {
-        final var board =
-            new BoardBuilder()
+        final var board = new BoardBuilder()
                 .withNumber(BingoNumber.fromString("23"))
                 .withNumber(BingoNumber.fromString("85"))
                 .withNumber(BingoNumber.fromString("246"))
                 .build();
         final var numberDrawn = BingoNumber.fromString("246");
 
-        final var numberMarked = numberDrawn.mark();
         board.mark(numberDrawn);
 
-        final var actual = board;
-
-        final var expected =
-            new BoardBuilder()
+        final var expected = new BoardBuilder()
                 .withNumber(BingoNumber.fromString("23"))
                 .withNumber(BingoNumber.fromString("85"))
-                .withNumber(numberMarked)
+                .withNumber(numberDrawn.mark())
                 .build();
 
-        assertEquals(expected, actual);
+        assertEquals(expected, board);
     }
 
 
