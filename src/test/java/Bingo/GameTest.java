61e1b3231ccb74b0e83a51b22a6909be1dfbd316
tommychat <thomas.bennett@caminu.fr>
2022-11-28 18:41:11 +0100

    a three number board with first number unmarked looses

--- src/test/java/Bingo/GameTest.java
+++ src/test/java/Bingo/GameTest.java
@@ -273,6 +273,23 @@ public class GameTest {
         assertEquals(BoardMessage.wins(), actual);
     }
 
+    @Test
+    void aThreeNumbersBoardWithFirstNumberUnmarkedLooses() {
+        final var board = new BoardBuilder()
+            .withNumber(BingoNumber.fromString("23"))
+            .withNumber(BingoNumber.fromString("135").mark())
+            .withNumber(BingoNumber.fromString("84").mark())
+            .build();
+
+        final var actual =
+            areAllNumbersMarkedInAOneNumberBoard(board) || areAllNumbersMarkedInATwoNumbersBoard(board) ||
+                (isNumberMarkedAt(board, 0) && isNumberMarkedAt(board, 2))
+                ? BoardMessage.wins()
+                : BoardMessage.looses();
+
+        assertEquals(BoardMessage.looses(), actual);
+    }
+
     static boolean isNumberMarkedAt(final Board board, final int index) {
         return board.numbers[index].toString().contains("marked");
     }
